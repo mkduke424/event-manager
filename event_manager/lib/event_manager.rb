b7bb99b0486abd9accf6bb95d1e89fcc9e99655b
miles <dkmk1149@gmail.com>
2020-07-18 02:37:01 -0400

    Connected API and pulled legislator data

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,34 @@
 require 'csv'
-puts "EventManager Initialized"
+require 'google/apis/civicinfo_v2'
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+                                    address: zip,
+                                    levels: 'country',
+                                    roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+  
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+
+  
+end
+
+puts "EventManager Initialized"
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
@@ -11,5 +36,8 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
